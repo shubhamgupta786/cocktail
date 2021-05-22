bfaae34e34cadefbd170f05ab47a1ecbb4875188
shubhamgupta786 <48391610+shubhamgupta786@users.noreply.github.com>
2021-05-22 17:01:16 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+       cocktail info........
+      </p>
+    </section>
+  );
+}
